28310961ee489d4cad72c2e8050ded5c968365ea
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-09-16 18:02:21 +0000

    countOnly function for the lotide project

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+// Function to check assertion
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢 Assertion Passed: ${actual} === ${expected} 🟢🟢`);
+  } else if (actual !== expected) {
+    console.log(`🔴🔴 Assertion Failed: ${actual} !== ${expected} 🔴🔴`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+}
+
+// Test Case:
+const firstNames = [
+  "Karl",
+  'Salima',
+  'Agouhanna',
+  'Fang',
+  'Kavith',
+  'Jason',
+  'Salima',
+  'Fang',
+  'Joe'
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, 'Fang': true, 'Agouhanna': false});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
