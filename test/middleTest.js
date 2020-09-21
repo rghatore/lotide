bd61fc7c3716c6b4d96bcd807514ac9f513b4716
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-09-21 20:29:16 +0000

    refactored tail and middle test files using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-//Test Case:
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']), ['d', 'e']);
+describe('#middle', () => {
+  it('returns [] for []', () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it('returns [] for [1, 2]', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it('returns [3] for [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns ['d', 'e'] for ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']", () => {
+    assert.deepEqual(middle(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']), ['d', 'e']);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,24 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-//Test Case: Manual
-console.log(tail([2, 'eh', 530]));
-console.log(tail([]));
-console.log(tail(['head']));
-
-//Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-//Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-//Test Case 2: Check the returned array elements
-const testCase2 = tail(["No tail!"]);
-assertEqual(testCase2.length, 0);
-assertEqual(testCase2[0], undefined);
-assertEqual(testCase2[1], undefined);
+describe('#tail', () => {
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it('returns empty array for ["No tail!"]', () => {
+    assert.deepEqual(tail(["No tail!"]), []);
+  });
+  it('returns empty array for an empty array', () => {
+    assert.deepEqual(tail([]), []);
+  })
+});
